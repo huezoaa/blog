d3fccd9558f3b910d9700572f3c0b6cf8e09a8a8
Angel Huezo <angel.huezo@kipusystems.com>
2016-01-31 23:26:59 -0500

    Going through the Routing section of the Rails guides. Entered fake values in routes.rb just to see what rake routes produced. Learned to use:  bundle exec rake routes and http://localhost:3000/rails/info/routes  Changes to be committed: 	modified:   README.rdoc 	modified:   config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
-  
+
   resources :articles do
     resources :comments
   end
- 
-  root 'welcome#index'
 
- 
+  root 'welcome#index'
+  #AH testing shit:
+  get '/warriors/:id', to: 'fighters#show'
+  get '/warriors/:id', to: 'fighters#show', as: 'guerrero'
+  get 'profile', to: 'users#show'
+  # get 'profile', to: :show #not working
+
 end
